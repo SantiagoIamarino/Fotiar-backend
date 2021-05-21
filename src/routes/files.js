7ed8a3bce938712c9232a66574fcc7f14c37cee8
I-FDesign <ifdesing.contacto@gmail.com>
2021-05-21 18:25:11 -0300

    Images uploading improved

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 
+const Jimp = require("jimp");
+
 const app = express();
 
 let storage = multer.diskStorage({
@@ -8,27 +10,82 @@ let storage = multer.diskStorage({
       cb(null, "C:/Users/santi/Desktop/Fotiar/backend/src/images/");
     },
     filename: (req, file, cb) => {
-      cb(null, file.originalname);
+        const fileName = new Date().getTime() + '-' + file.originalname;
+      cb(null, fileName);
     },
 });
 
 let uploadFile = multer({
     storage: storage,
-}).any("uploads");
+}).single("uploads");
+
+function generateCopy(file) {
+    return new Promise((resolve, reject) => {
+        const LOGO = "C:/Users/santi/Desktop/Fotiar/backend/src/logo.png";
+
+        const LOGO_MARGIN_PERCENTAGE = 5;
+    
+        const main = async () => {
+            const [image, logo] = await Promise.all([
+                Jimp.read(file.path),
+                Jimp.read(LOGO)
+            ]);
+    
+            logo.resize(image.bitmap.width / 8, Jimp.AUTO);
+    
+            const xMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+            const yMargin = (image.bitmap.width * LOGO_MARGIN_PERCENTAGE) / 100;
+    
+            const X = image.bitmap.width - logo.bitmap.width - xMargin;
+            const Y = image.bitmap.height - logo.bitmap.height - yMargin;
+    
+            return image
+                .composite(logo, X, Y, [{
+                    mode: Jimp.BLEND_SCREEN,
+                    opacitySource: 0.1,
+                    opacityDest: 1
+                }])
+                .quality(30)
+                .resize(
+                    Math.round(image.bitmap.width / 10),
+                    Math.round(image.bitmap.height / 10)
+                );
+        };
+    
+        main().then(image => { 
+            const copyFileName = 'copy-' + file.filename;
+            image.write('./src/images/' + copyFileName);
+            resolve(copyFileName);
+        });
+    })
+    
+}
 
 
 app.post('/', (req, res) => {
 
     uploadFile(req, res, (err) => {
-        if (err instanceof multer.MulterError) {
-            console.log(err); //Multer error
-        } else if (err) {
-            console.log(err); //Some other error
+        if(err) {
+            return res.status(500).json({
+                ok: false,
+                error: err
+            })
         }
 
-        return res.status(200).json({
-            ok: true,
-            message: 'Imagenes subidas correctamente'
+        const fileName = req.file.filename;
+
+        generateCopy(req.file).then((copyFileName) => {
+            return res.status(200).json({
+                ok: true,
+                message: 'Imagenes subidas correctamente',
+                fileName,
+                copyFileName
+            })
+        }).catch((error) => {
+            return res.status(500).json({
+                ok: false,
+                error
+            })
         })
     })
 })
