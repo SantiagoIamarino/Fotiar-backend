03bee7f1f0dbe213c0617f967fb4abbb6ec59b46
I-FDesign <ifdesing.contacto@gmail.com>
2021-06-15 20:20:51 -0300

    Production images path

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,5 +1,5 @@
 module.exports = {
     jwtKey: '!J$SO&AS&DAN_CA3STOR_FO1TIA31R$$$_ASDF5A!$!',
-    filesPath: 'C:/Users/santi/Desktop/Fotiar/backend/src/images/',
-    // filesPath: '/var/images/',
+    // filesPath: 'C:/Users/santi/Desktop/Fotiar/backend/src/images/',
+    filesPath: '/var/images/',
 }
